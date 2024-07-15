d5a8aace7a50c924bbe75d4402ef1f3a8011f811
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2024-07-15 14:32:20 +0300

    add comments to describe operation

--- src/algorithms-&-data-structures/big-o.ts
+++ src/algorithms-&-data-structures/big-o.ts
@@ -1,11 +1,15 @@
+// the number of operation is important EX: (+, *,/,...etc)
+
 export function addUpTo(n: number): number {
   let total: number = 0;
   for (let i = 1; i <= n; i++) {
-    total += i;
+    // here we multiple operations, i++ is going to happen depend on the number of the loop going to happen
+    total += i; // here the operation that going to happen depend on n, if n = 5 then 5 operations if n = 1000 then 1000 operation
   }
   return total;
+  // conclusion here is the operation numbers depend on the n but on addUpTo2 it is always going to be 3
 }
 
 export function addUpTo2(n: number) {
-  return (n * (n + 1)) / 2;
+  return (n * (n + 1)) / 2; // here we only have 3 operations which is *,+ and / no matter of n it is always going to be 3
 }
